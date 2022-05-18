1eac246412dce4a896268f08c10f32d7ce8afcf9
Alberto Lagos T <alberto.lagos@modmed.com>
2022-05-18 12:08:51 -0400

    Update dependencies version.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
   name: "Stencil",
   dependencies: [
-    .package(url: "https://github.com/kylef/PathKit.git", from: "0.8.0"),
-    .package(url: "https://github.com/kylef/Spectre.git", from: "0.7.0")
+    .package(url: "https://github.com/kylef/PathKit.git", from: "0.9.0"),
+    .package(url: "https://github.com/kylef/Spectre.git", from: "0.8.0")
   ]
 )
